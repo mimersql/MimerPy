a8424cedd2953f2b8471f2e77c50acce8da3e009
Per Schröder <per.schroder@mimer.com>
2018-12-22 19:02:51 +0100

    Updated dropdb.py so it clears the new test environment

--- tests/dropdb.py
+++ tests/dropdb.py
@@ -1,23 +1,9 @@
 import mimerpy
-import unittest
-import time
-import random
 
-from mimerpy.mimPyExceptions import *
 import db_config
 
-print("Dropping")
-a = mimerpy.connect(dsn = db_config.DB_INFO['DBNAME'], user =
-            db_config.DB_INFO['SYSADM'], password = db_config.DB_INFO['SYADMPSW'])
-b = a.cursor()
-try:
-    b.execute("DROP DATABANK testbank CASCADE")
-except:
-    "nothing"
-try:
-    b.execute("DROP IDENT TEST_IDENT CASCADE")
-except:
-    "nothing"
-b.close()
-a.close()
-print("Dropped")
+if __name__ == '__main__':
+    with mimerpy.connect(**db_config.SYSUSR) as syscon:
+        with syscon.cursor() as c:
+            c.execute("drop ident mimerpy cascade")
+        syscon.commit()
