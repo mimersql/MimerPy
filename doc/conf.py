84f2b7bb99c0187a143a8a87a04554df4129a852
Erik <erik.gunne@mimer.com>
2021-03-06 16:32:27 +0100

    clean up in config

--- doc/conf.py
+++ doc/conf.py
@@ -1,4 +1,3 @@
-from recommonmark.parser import CommonMarkParser
 # -*- coding: utf-8 -*-
 #
 # MimerPy documentation build configuration file, created by
@@ -29,7 +28,7 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autosectionlabel', 'recommonmark']
+extensions = ['sphinx.ext.autosectionlabel',]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
